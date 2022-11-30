a502659d519be970d641bb7023ce90bb2de5ba9c
Abhiram <abhiraam2004@gmail.com>
2022-11-30 22:58:23 +0530

    changed elements in template for my website

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ const siteMeta = {
 	description: "An opinionated starter theme for Astro",
 	lang: "en-GB",
 	ogLocale: "en_GB",
-	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+	githubUrl: "https://github.com/Abhiram031/abhiram031.github.io",
 	themeColorLight: "#fafafa",
 	themeColorDark: "#1d1f21",
 };
